ec003e897af3aa87d3efa2a955006f7026b4b795
Aarif <mohamedaarif1811@gmail.com>
2025-08-05 19:29:27 +0530

    Now changed

--- src/test.rs
+++ src/test.rs
@@ -8,13 +8,20 @@ fn memory() {
     fruits.insert(0, "apple");
     fruits.insert(0, "apple");
     fruits.insert(0, "apple");
-      fruits.insert(0, "apple");
+     
     fruits.insert(0, "apple");
     fruits.insert(0, "apple");
-  println!("{:?}", fruits);
-  println!("{:?}", fruits);
+    fruits.insert(0, "apple");fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
   println!("Length: {}, Capacity: {}", fruits.len(), fruits.capacity());
         println!("Start memory address (heap buffer): {:p}", fruits.as_ptr());
   }
- 
+ // changed ->
   
