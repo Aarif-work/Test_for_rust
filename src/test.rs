552f3202d998e38839089e239329c9198c6bc59c
sanjeevan197 <sanjeev.arjava@gmail.com>
2025-08-05 19:03:48 +0530

    test.king

---
+++ src/test.rs
@@ -0,0 +1,20 @@
+fn memory() {
+    let mut fruits = vec!["banana", "orange"];
+    fruits.insert(0, "apple");
+
+    println!("Length: {}, Capacity: {}", fruits.len(), fruits.capacity());
+        println!("Start memory address (heap buffer): {:p}", fruits.as_ptr());
+          fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+      fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+    fruits.insert(0, "apple");
+  println!("{:?}", fruits);
+  println!("{:?}", fruits);
+  println!("Length: {}, Capacity: {}", fruits.len(), fruits.capacity());
+        println!("Start memory address (heap buffer): {:p}", fruits.as_ptr());
+  }
+ 
+  
