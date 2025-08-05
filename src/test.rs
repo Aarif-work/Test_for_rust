5c6d08b1766385495d66f5b2583bdd633432ba8d
sanjeevan197 <sanjeev.arjava@gmail.com>
2025-08-05 19:32:40 +0530

    sanjeevan

--- src/test.rs
+++ src/test.rs
@@ -20,21 +20,6 @@ fn memory() {
     fruits.insert(0, "apple");fruits.insert(0, "apple");
     fruits.insert(0, "apple");
     fruits.insert(0, "apple");
-<<<<<<< HEAD
-=======
-  println!("{:?}", fruits);
-  println!("{:?}", fruits);
-  fruits.insert(0, "apple");
-  fruits.insert(0, "apple");
-  fruits.insert(0, "apple");
-  println!("{:?}", fruits);
-  println!("{:?}", fruits);
-  fruits.insert(0, "apple");
-  fruits.insert(0, "apple");
-  fruits.insert(0, "apple");
-  println!("{:?}", fruits);
-  //sanjeev add
->>>>>>> 2de14f6 (sanjeevan)
   println!("Length: {}, Capacity: {}", fruits.len(), fruits.capacity());
         println!("Start memory address (heap buffer): {:p}", fruits.as_ptr());
   }
